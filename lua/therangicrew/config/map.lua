1ddcd0c901a7ffc29fc02bea826a0414ebbf27f8
TheRangiCrew <ryan.z.adam@gmail.com>
2024-10-18 10:24:57 +1300

    updated maps

--- lua/therangicrew/config/map.lua
+++ lua/therangicrew/config/map.lua
@@ -1,9 +1,12 @@
--- Set the leader key
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+-- Se the leader key
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- Instead of needing to press escape all the time
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
 -- Format
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+-- Shorthand to open the netrw
+vim.keymap.set("n", "<leader>ex", ":Explore<Enter>")
