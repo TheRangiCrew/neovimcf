d28a423db6d251048269bc1d6bf5e3e6bac1f3cd
TheRangiCrew <ryan.z.adam@gmail.com>
2024-12-02 12:01:00 +1300

    Added keymaps

--- lua/therangicrew/plugins/telescope.lua
+++ lua/therangicrew/plugins/telescope.lua
@@ -6,7 +6,8 @@ return {
 		require("telescope").setup({})
 
 		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
-		vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+		vim.keymap.set("n", "<leader>gf", builtin.git_files, {})
+		vim.keymap.set("n", "<leader>gr", builtin.live_grep, {})
 	end,
 }
--- lua/therangicrew/plugins/trouble.lua
+++ lua/therangicrew/plugins/trouble.lua
@@ -1,12 +1,10 @@
 return {
-    "folke/trouble.nvim",
-    config = function()
-        require("trouble").setup({
-            icons = true,
-        })
+	"folke/trouble.nvim",
+	config = function()
+		require("trouble").setup({
+			-- icons = true,
+		})
 
-        vim.keymap.set("n", "<leader>tt", function()
-            require("trouble").toggle()
-        end)
-    end,
+		vim.keymap.set("n", "<leader>tt", "<cmd>Trouble diagnostics toggle<cr>")
+	end,
 }
